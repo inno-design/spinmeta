e150cd1794b6f67ba20d735414742a35922f3ed2
eunsun <parang4si@innogrid.com>
2023-03-07 14:02:58 +0900

    mobile

--- src/resources/js/nav.js
+++ src/resources/js/nav.js
@@ -1,15 +1,3 @@
 const navToggle = function (){
-  document.querySelector('.header_nav').classList.toggle('open');
+  document.querySelector('.header').classList.toggle('open');
 }
-
-$(document).ready(function() {
-  const $btn = $('.btn_subnav');
-
-  $btn.each(function(){
-    const $target = $(this);
-    $(this).on('click',function(){
-      $('.gnb_main').find('.gnb_main_item').removeClass('open');
-      $target.parent('.gnb_main_item').addClass('open')
-    });
-  });
-});
